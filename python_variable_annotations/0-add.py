78791086016d9207372bace1e21466214e571574
Hayama0024 <hayamareizo11@gmail.com>
2025-07-28 13:22:22 +0000

    fix codestyle

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Module with a function that adds two floats."""
 
+
 def add(a: float, b: float) -> float:
     """Returns the sum of two float numbers."""
     return a + b
