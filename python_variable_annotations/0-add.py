25e71f818f6f6d7cc781f5550bdf8b9eef73987f
Hayama0024 <hayamareizo11@gmail.com>
2025-07-28 13:21:04 +0000

    first task with README

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""Module with a function that adds two floats."""
+
+def add(a: float, b: float) -> float:
+    """Returns the sum of two float numbers."""
+    return a + b
