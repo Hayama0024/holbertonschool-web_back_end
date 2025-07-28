74e9f33777ef61e67aae4c756c3124dd4cd6902f
Hayama0024 <hayamareizo11@gmail.com>
2025-07-28 13:34:34 +0000

    task2

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Module that provides a function to return the floor of a float."""
+
+
+import math
+
+
+def floor(n: float) -> int:
+    """Returns the floor of the float n."""
+    return math.floor(n)
