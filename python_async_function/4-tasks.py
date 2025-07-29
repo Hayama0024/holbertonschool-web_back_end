668d4bd73389efc3061d41dcf77b8dea0e68cd94
Hayama0024 <hayamareizo11@gmail.com>
2025-07-29 12:46:25 +0000

    all tasks

---
+++ python_async_function/4-tasks.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Module that spawns multiple asyncio tasks using task_wait_random."""
+
+import asyncio
+from typing import List
+task_wait_random = __import__('3-tasks').task_wait_random
+
+
+async def task_wait_n(n: int, max_delay: int) -> List[float]:
+    tasks = [task_wait_random(max_delay) for _ in range(n)]
+    delays = []
+    for completed in asyncio.as_completed(tasks):
+        result = await completed
+        delays.append(result)
+    return delays
