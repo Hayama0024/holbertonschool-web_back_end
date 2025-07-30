d13cb2775500a83aba2c09eb9e7bea32ec656aa4
Hayama0024 <hayamareizo11@gmail.com>
2025-07-30 09:24:08 +0000

    all tasks

--- python_async_comprehension/2-measure_runtime.py
+++ python_async_comprehension/2-measure_runtime.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python3
-"""Measure the total runtime of four parallel async comprehensions."""
-
+"""
+2-measure_runtime.py
+Module containing an asynchronous coroutine.
+"""
+from typing import Generator, List
+import random
 import asyncio
 import time
-from typing import List
-from 1-async_comprehension import async_comprehension
+
+async_comprehension = __import__('1-async_comprehension').async_comprehension
 
 
 async def measure_runtime() -> float:
-    """Execute async_comprehension 4 times and return the runtime."""
-    start = time.perf_counter()
-    await asyncio.gather(*(async_comprehension() for _ in range(4)))
-    end = time.perf_counter()
+    """
+    Computes runtime for four concurrent coroutines.
+    """
+    start: float = time.time()
+    await asyncio.gather(*[async_comprehension() for _ in range(4)])
+    end: float = time.time()
+
     return end - start
