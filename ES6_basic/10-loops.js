32be700361b18831e8ae54ecca3a31bb548139a2
Hayama0024 <hayamareizo11@gmail.com>
2025-08-23 16:22:54 +0000

    all tasks

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,7 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-    for (const [idx, value] of array.entries()) {
-      array[idx] = appendString + value;
-    }
-    return array;
+  const result = [];
+  for (const value of array) {
+    result.push(appendString + value);
   }
-  
+
+  return result;
+}
